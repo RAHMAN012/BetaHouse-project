df6be044541f37a0d2600bfb2e0dfb8b7d50bb48
Taiwo Adepoju <rahmanadepoju03@gmail.com>
2025-02-28 14:42:43 +0100

    fourth commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 module.exports = {
     content: [      "./index.html",      "./src/**/*.{js,ts,jsx,tsx}",  ],
-    content: ["./node_modules/flyonui/dist/js/accordion.js"],
     theme: {
       extend: {},
     },
     plugins: [
-        require("flyonui"),
+       
     ],
   };
